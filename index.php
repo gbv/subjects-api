ed83cca63b8b81b8c119e26b085647e8a26494b5
Jakob Voss <voss@gbv.de>
2018-05-25 14:00:44 +0200

    Log results for caching

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ if (!$_GET) {
 }
 
 // lists of URIs separated by whitespace or '|'
-$inScheme = array_filter(preg_split('/[\s|]+/', $_GET['inScheme'] ?? ''));
+$schemes  = array_filter(preg_split('/[\s|]+/', $_GET['schemes'] ?? ''));
 $members  = array_filter(preg_split('/[\s|]+/', $_GET['members'] ?? ''));
 $database = array_filter(preg_split('/[\s|]+/', $_GET['database'] ?? ''));
 
@@ -71,13 +71,29 @@ function getNumberOfRecordsViaSRU($sru, $cqlkey, $id) {
 
 // get number of records via SRU
 $count = getNumberOfRecordsViaSRU($sru, $cqlkey, $id);
-response([
+$time  = date("c",time());
+
+$occurrence = [
     "database"  => [ "uri" => $database ],
     "memberSet" => [ [ "uri" => $uri ] ],
     "count"     => $count,
-    "modified"  => date("Y-m-d",time()),
+    "modified"  => $time,
     "url"       => 
         "https://gso.gbv.de/DB=2.1/CMD?ACT=SRCHA&IKT=1016&SRT=YOP&TRM="
             .urlencode("$cqlkey $id")
-]);
+];
 
+function logOccurrence($occ, $file) {
+    $row = array_map(function ($member) { 
+        return $member['uri']; }, 
+        $occ['memberSet']
+    );
+    array_unshift($row, $occ['count']);
+    array_unshift($row, $occ['modified']);
+    array_unshift($row, $occ['database']['uri']);
+    file_put_contents($file, implode(" ", $row), FILE_APPEND);
+}
+
+logOccurrence($occurrence, 'occurrences.txt');
+
+response($occurrence);

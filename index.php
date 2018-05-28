6a699c429456c7e97c81207b56eb5838cc3baf9a
Jakob Voss <voss@gbv.de>
2018-05-28 14:31:52 +0200

    Prepare retrieval of co-occurrences

--- index.php
+++ index.php
@@ -5,17 +5,21 @@ include_once 'utils.php';
 $config = load('config.json');
 
 if (!$_GET) {
-    error('400', 'missing query parameters', [ 'examples' => $config['examples'] ]);
+    error('400', 'missing query parameters', [ 'schemes' => $config['schemes'] ] );
 }
 
 // lists of URIs separated by whitespace or '|'
-$schemes  = array_filter(preg_split('/[\s|]+/', $_GET['schemes'] ?? ''));
 $members  = array_filter(preg_split('/[\s|]+/', $_GET['members'] ?? ''));
-$database = array_filter(preg_split('/[\s|]+/', $_GET['database'] ?? ''));
+
+if (!$members) {
+	error(400, 'missing query parameter members (space-separated list of URIs)');
+} elseif (count($members) > 1) {
+    error(400, 'occurrences with multiple members not supported yet');
+}
 
 // detect concept schemes that members origin from
 $members = array_map(function($uri) use ($config) {
-    foreach ($config['schemes'] as $scheme) {        
+    foreach ($config['schemes'] as $scheme) {
         if ($scheme['namespace'] && startswith($uri, $scheme['namespace'])) {
             return [
                 'uri'      => $uri,
@@ -26,35 +30,62 @@ $members = array_map(function($uri) use ($config) {
 	error(404, "failed to detect concept scheme of URI $uri");
 }, $members);
 
-if (!$members) {
-	error(400, 'missing query parameter members (space-separated list of URIs)');
+// lists of URIs separated by whitespace or '|'
+$schemes = array_filter(preg_split('/[\s|]+/', $_GET['schemes'] ?? ''));
+$tmp = [];
+foreach ($config['schemes'] as $scheme) {
+    if (in_array($scheme['uri'], $schemes)) {
+        $tmp[$scheme['uri']] = $scheme;
+    }
 }
+$schemes = $tmp;
+unset($schemes[ $members[0]['inScheme'][0]['uri'] ]);
 
-if (count($members)>1) {
-	error(400, 'occurrences with multiple members not supported yet');
+foreach ($schemes as $scheme) {
+       if (!isset($scheme['PICAPATH'])) {
+        error(400, "co-occurrence with ".$scheme['uri']." not supported yet");
+    }
 }
 
 // TODO: customize database
-$database = "http://uri.gbv.de/database/gvk";
-$sru      = "http://sru.gbv.de/gvk";
+$database = array_filter(preg_split('/[\s|]+/', $_GET['database'] ?? ''));
+$dbkey = 'gvk';
 
-// query one concept
-$concept = $members[0];
-$uri     = $concept['uri'];
-$scheme  = $concept['inScheme'][0];
-$cqlkey  = $scheme['CQLKEY'];
+function getSimpleOccurrence($concept, $dbkey) {
+    $uri     = $concept['uri'];
+    $scheme  = $concept['inScheme'][0];
+    $cqlkey  = $scheme['CQLKEY'];
 
-$id = substr($uri, strlen($scheme['namespace']));
+    $id = substr($uri, strlen($scheme['namespace']));
 
-if ($cqlkey == 'ddc') {
-    if (startswith($id, 'class/')) {
-        $id = substr($id, strlen('class/'));
-    } else {
-        error(400, "DDC URI not supported: $uri");
+    if ($cqlkey == 'ddc') {
+        if (startswith($id, 'class/')) {
+            $id = substr($id, strlen('class/'));
+        } else {
+            error(400, "DDC URI not supported: $uri");
+        }
+    } elseif ($cqlkey == 'rvk' || $cqlkey == 'kab') {
+        $id = preg_replace('/_/',' ',$id);
+        $id = preg_replace('/-/',' - ',$id);
     }
-} elseif ($cqlkey == 'rvk') {
-    $id = preg_replace('/_/',' ',$id);
-    $id = preg_replace('/-/',' - ',$id);
+
+    $count = getNumberOfRecordsViaSRU("http://sru.gbv.de/$dbkey", $cqlkey, $id);
+    $time  = date("c",time());
+
+    return [
+        "database"  => [ "uri" => "http://uri.gbv.de/database/$dbkey" ],
+        "memberSet" => [ 
+            [ 
+                "uri" => $uri,
+                "inScheme" => [ [ 'uri' => $scheme['uri'] ] ]
+            ] 
+        ],
+        "count"     => $count,
+        "modified"  => $time,
+        "url"       =>
+            "https://gso.gbv.de/DB=2.1/CMD?ACT=SRCHA&IKT=1016&SRT=YOP&TRM="
+                .urlencode("$cqlkey $id")
+    ];
 }
 
 function getNumberOfRecordsViaSRU($sru, $cqlkey, $id) {
@@ -69,31 +100,25 @@ function getNumberOfRecordsViaSRU($sru, $cqlkey, $id) {
     }
 }
 
-// get number of records via SRU
-$count = getNumberOfRecordsViaSRU($sru, $cqlkey, $id);
-$time  = date("c",time());
-
-$occurrence = [
-    "database"  => [ "uri" => $database ],
-    "memberSet" => [ [ "uri" => $uri ] ],
-    "count"     => $count,
-    "modified"  => $time,
-    "url"       => 
-        "https://gso.gbv.de/DB=2.1/CMD?ACT=SRCHA&IKT=1016&SRT=YOP&TRM="
-            .urlencode("$cqlkey $id")
-];
-
 function logOccurrence($occ, $file) {
-    $row = array_map(function ($member) { 
-        return $member['uri']; }, 
+    $row = array_map(function ($member) {
+        return $member['uri']; },
         $occ['memberSet']
     );
     array_unshift($row, $occ['count']);
     array_unshift($row, $occ['modified']);
     array_unshift($row, $occ['database']['uri']);
-    file_put_contents($file, implode(" ", $row), FILE_APPEND);
+    file_put_contents($file, implode(" ", $row)."\n", FILE_APPEND);
 }
 
-logOccurrence($occurrence, 'occurrences.txt');
+$occurrences = array_map(function ($member) use ($dbkey) {
+    $occ = getSimpleOccurrence($member, $dbkey);
+    logOccurrence($occ, 'occurrences.txt');
+    return $occ;
+}, $members);
 
-response($occurrence);
+if (count($schemes)) {
+    # TODO: get records and count in PICAPATH
+}
+
+response($occurrences);

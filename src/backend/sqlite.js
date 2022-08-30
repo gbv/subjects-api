b7ce37380036fee14a2928406a6c9cdd7a4091bd
Jakob Voss <voss@gbv.de>
2022-08-30 15:35:20 +0200

    SQLite: add method to update/delete record

--- src/backend/sqlite.js
+++ src/backend/sqlite.js
@@ -37,6 +37,16 @@ CREATE TABLE metadata (
     return this.db.prepare(`SELECT b.voc, b.notation, count(*) AS freq FROM subjects AS b JOIN (SELECT ppn FROM subjects WHERE voc = ? AND notation = ?) a ON a.ppn = b.ppn WHERE b.voc ${otherScheme ? "=" : "!="} ? GROUP BY b.voc, b.notation HAVING count(*) >= ? ORDER BY freq DESC LIMIT 10;`).all([scheme.VOC, notation, otherScheme ? otherScheme.VOC : scheme.VOC, threshold])
   }
 
+  async updateRecord(ppn, rows=[]) {
+    const insert = this.db.prepare("INSERT INTO subjects (ppn, voc, notation) VALUES (@ppn, @voc, @notation)")
+
+    // NOTE: we may use transaction here unless bulk import
+    this.db.prepare("DELETE FROM subjects WHERE ppn = ?").run(ppn)
+    for (const row of rows) {
+      insert({ ...row, ppn })
+    }
+  }
+
   async metadata() {
     const { occCount } = await this.db.prepare("SELECT COUNT(*) AS occCount FROM subjects").get()
     const { recCount } = await this.db.prepare("SELECT COUNT(DISTINCT ppn) AS recCount FROM subjects").get()

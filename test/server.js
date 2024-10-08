447f116317b65e4fedb935e9615f59f5a9bbaadc
Stefan Peters <Stefan.Peters@gbv.de>
2024-10-08 12:29:09 +0200

    Update dependencies
    Adjusted usage of chai in tests accordingly.

--- test/server.js
+++ test/server.js
@@ -1,6 +1,4 @@
-import chai from "chai"
-import chaiHttp from "chai-http"
-chai.use(chaiHttp)
+import chai from "./chai.js"
 import assert from "node:assert"
 
 const { appStarted } = await import("../server.js")
@@ -8,24 +6,24 @@ const app = await appStarted
 
 describe("server", () => {
   it("/ should return HTML",
-    () => chai.request(app).get("/")
+    () => chai.request.execute(app).get("/")
       .then(res => {
         assert.equal(res.status,200)
       }))
   it("/status should return JSON",
-    () => chai.request(app).get("/status")
+    () => chai.request.execute(app).get("/status")
       .then(res => {
         assert.equal(res.status,200)
         assert.ok(res.body.ok)
       }))
   it("/voc should return vocabularies",
-    () => chai.request(app).get("/voc")
+    () => chai.request.execute(app).get("/voc")
       .then(res => {
         assert.equal(res.status,200)
         assert.equal(res.body.length,16)
       }))
   it("/databases should return databases",
-    () => chai.request(app).get("/databases")
+    () => chai.request.execute(app).get("/databases")
       .then(res => {
         assert.equal(res.status,200)
         assert.equal(res.body.length,1)
